2be8dc78dfd4fb703504459a4fa185811c94806c
Om Kapadiya <omkapadiya34@gmail.com>
2025-08-17 01:16:55 +0530

    fix errros

--- obj/Debug/net8.0/LedgerLink.AssemblyInfo.cs
+++ obj/Debug/net8.0/LedgerLink.AssemblyInfo.cs
@@ -13,8 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LedgerLink")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26ce1f4d5c87aa61c46cc6f0f6bb0efad0a48b8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b0af938d2cab1ed7bf1e47caa5649c9e1520394")]
 [assembly: System.Reflection.AssemblyProductAttribute("LedgerLink")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LedgerLink")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
