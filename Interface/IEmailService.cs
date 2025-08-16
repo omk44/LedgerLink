776ffe40adc909b0a23cb45474a094eb572d6113
Om Kapadiya <omkapadiya34@gmail.com>
2025-08-16 21:25:52 +0530

    feat:email service reminder added

---
+++ Interface/IEmailService.cs
@@ -0,0 +1,10 @@
+// Path: LedgerLink/Interface/IEmailSmsService.cs
+using System.Threading.Tasks;
+
+namespace LedgerLink.Interface
+{
+    public interface IEmailService
+    {
+        Task<bool> SendEmailAsync(string toEmail, string subject, string message);
+    }
+}
