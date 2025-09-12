98ce77fac17950392f67755af6df9f817c86efed
Om Kapadiya <omkapadiya34@gmail.com>
2025-09-12 23:37:58 +0530

    feat:pagging concept added with controller action and table of tansaction and pyment

--- ViewModels/CustomerDetailsViewModel.cs
+++ ViewModels/CustomerDetailsViewModel.cs
@@ -1,15 +1,22 @@
+// Path: LedgerLink/ViewModels/CustomerDetailsViewModel.cs
 using System.Collections.Generic;
+using System.Linq;
 using LedgerLink.Models;
+using X.PagedList;
 
 namespace LedgerLink.ViewModels
 {
     public class CustomerDetailsViewModel
     {
         public Customer Customer { get; set; } = null!;
+
         public IEnumerable<Product> Products { get; set; } = new List<Product>();
-        public IEnumerable<Transaction> Transactions { get; set; } = new List<Transaction>();
-        public IEnumerable<Payment> Payments { get; set; } = new List<Payment>();
+
+        // Use StaticPagedList for safe empty defaults (no need to call ToPagedList here)
+        public IPagedList<Transaction> Transactions { get; set; } 
+            = new StaticPagedList<Transaction>(Enumerable.Empty<Transaction>(), 1, 10, 0);
+
+        public IPagedList<Payment> Payments { get; set; } 
+            = new StaticPagedList<Payment>(Enumerable.Empty<Payment>(), 1, 10, 0);
     }
 }
-
-//it will help to all necessary data to the CustomerDetails.cshtml view.
