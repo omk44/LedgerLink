1b406faa242cbeae54c260b52767a33efde03af1
Om Kapadiya <omkapadiya34@gmail.com>
2025-09-14 00:20:30 +0530

    feat:payment id can be nullable

--- Interface/IPaymentRepo.cs
+++ Interface/IPaymentRepo.cs
@@ -5,7 +5,7 @@ namespace LedgerLink.Interface
     public interface IPaymentRepo
     {
         IEnumerable<Payment> GetAllPayments();
-        Payment GetPaymentById(Guid id);
+        Payment? GetPaymentById(Guid id);
         Payment AddPayment(Payment payment);
         Payment UpdatePayment(Payment payment);
         Payment DeletePayment(Guid id);
